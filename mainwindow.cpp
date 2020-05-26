03e444c9a164f5167f02d4b0f74769511a9348d6
HermesSJC <369348508@qq.com>
2020-05-26 18:44:19 +0800

    add some code 1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -6,27 +6,51 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //初始化一些变量
+    dateTime = new QDateTime();
+    serialPort = new QSerialPort(this);
+
+    //隐藏断开串口
+    ui->serialDisconnectButton->setDisabled(true);
 }
 
 MainWindow::~MainWindow()
 {
+    delete dateTime;
+
     delete ui;
 }
 
 
 void MainWindow::on_serialSettingsAction_triggered()
 {
+    //如果是隐藏的 就显示
+    if(ui->serialSettingsDockWidget->isHidden())
+    {
+        ui->serialSettingsDockWidget->show();
+    }
+
 
 }
 
 void MainWindow::on_systemSettingsAction_triggered()
 {
-
+    //如果是隐藏的 就显示
+    if(ui->systemSettingsDockWidget->isHidden())
+    {
+        ui->systemSettingsDockWidget->show();
+    }
 }
 
 void MainWindow::on_serialConnectButton_clicked()
 {
-
+    //判断是否有串口
+    if(ui->serialPortComboBox->count() == 0)
+    {
+        statusBar()->showMessage(getCurrentTime() + QStringLiteral(" 没有发现串口"), 2000);
+        return;
+    }
 }
 
 void MainWindow::on_serialDisconnectButton_clicked()
@@ -35,6 +59,61 @@ void MainWindow::on_serialDisconnectButton_clicked()
 }
 
 void MainWindow::on_serialRefreshButton_clicked()
+{
+    //清除原有内容
+    ui->serialPortComboBox->clear();
+
+    //扫描现有串口 判断是否非空
+    const auto infos = QSerialPortInfo::availablePorts();
+    if(infos.isEmpty())
+    {
+        statusBar()->showMessage(getCurrentTime() + QStringLiteral(" 没有发现串口"), 2000);
+        return;
+    }
+
+    //非空就添加串口
+    for(const QSerialPortInfo &info : infos)
+    {
+        ui->serialPortComboBox->addItem(info.portName());
+    }
+}
+
+void MainWindow::on_serialClearSendButton_clicked()
+{
+
+}
+
+void MainWindow::on_serialClearReceiveSendButton_clicked()
+{
+
+}
+
+void MainWindow::on_serialClearReceiveButton_clicked()
+{
+
+}
+
+void MainWindow::on_serialSingleSendButton_clicked()
+{
+
+}
+
+void MainWindow::on_isNewLineCheckBox_toggled(bool checked)
+{
+
+}
+
+void MainWindow::on_isAddTimeCheckBox_toggled(bool checked)
+{
+
+}
+
+void MainWindow::on_chooseReferencePathFileButton_clicked()
+{
+
+}
+
+void MainWindow::on_chooseAutorunPathFileButton_clicked()
 {
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,19 @@
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+
+#include <QString>
+#include <QFile>
+#include <QDateTime>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QFileDialog>
+#include <QTimerEvent>
+#include <QDir>
+#include <QTimer>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,6 +27,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private:
+
+    QDateTime *dateTime;
+    inline QString getCurrentTime(const QString &format = "hh:ss:mm") { return dateTime->currentDateTime().toString(format); }
+
+    QSerialPort *serialPort;
+
 private slots:
     void on_serialSettingsAction_triggered();
 
@@ -25,6 +44,22 @@ private slots:
     void on_serialDisconnectButton_clicked();
 
     void on_serialRefreshButton_clicked();
+
+    void on_serialClearSendButton_clicked();
+
+    void on_serialClearReceiveSendButton_clicked();
+
+    void on_serialClearReceiveButton_clicked();
+
+    void on_serialSingleSendButton_clicked();
+
+    void on_isNewLineCheckBox_toggled(bool checked);
+
+    void on_isAddTimeCheckBox_toggled(bool checked);
+
+    void on_chooseReferencePathFileButton_clicked();
+
+    void on_chooseAutorunPathFileButton_clicked();
 
 private:
     Ui::MainWindow *ui;
